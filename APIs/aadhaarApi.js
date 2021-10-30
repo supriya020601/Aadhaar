7358591d83bd251353e6617372d54b932add54c0
supriya020601 <supriyarachakonda001@gmail.com>
2021-10-30 12:49:17 +0530

    fourth commit

--- APIs/aadhaarApi.js
+++ APIs/aadhaarApi.js
@@ -3,7 +3,8 @@ const exp=require("express")
 const userApi=exp.Router();
 const expressErrorHandler=require("express-async-handler")
 const bcryptjs=require("bcryptjs")
-const jwt=require("jsonwebtoken")
+const jwt=require("jsonwebtoken");
+
 
 
 //add body parser middleware
@@ -14,11 +15,11 @@ userApi.use(exp.json())
 //create aadhaaruser using async and await method
 userApi.post('/createaadhaar',expressErrorHandler(async (req,res,next)=>{
     let aadhaarCollectionObj = req.app.get("userAddressCollectionObject")
-    //get userObj
+   
     let newUser=req.body;
-    //search for existing user
+   
     let user=await aadhaarCollectionObj.findOne({Aadhaar:newUser.Aadhaar})
-    //if user existed
+   
     if(user!=null){
         res.send({message:"user already existed"})
     }
@@ -34,25 +35,48 @@ userApi.get("/getaadhaar/:obj",expressErrorHandler(async (req,res,next)=>{
     let aadhaarCollectionObj = req.app.get("userAddressCollectionObject")
     //get userObj
    let newUser=req.params.obj;
-    console.log(newUser)
+   
     //search for existing user
     let user=await aadhaarCollectionObj.findOne({Aadhaar:newUser})
     //if user existed
-    console.log(user)
+   
     if(user!=null){
 
-        if(user.Village.toLowerCase()===user.Subdistrict.toLowerCase() && user.Subdistrict.toLowerCase()===user.District.toLowerCase()){
+        user.Village=user.Village.toLowerCase()
+        user.Subdistrict=user.Subdistrict.toLowerCase()
+        user.District=user.District.toLowerCase()
+        user.Street=user.Street.toLowerCase()
+        user.Area=user.Area.toLowerCase()
+
+
+        if( user.Village===user.Subdistrict && user.Subdistrict===user.District){
             user.Subdistrict=null;
             user.District=null;
         }
-        else if(user.Village.toLowerCase()===user.Subdistrict.toLowerCase()){
+        else if(user.Village===user.Subdistrict){
                 user.Subdistrict=null;
                 
         }
-        else if(user.District.toLowerCase()===user.Subdistrict.toLowerCase()){
+        else if(user.District===user.Subdistrict){
             user.District=null;
         }
 
+
+        let str=user.Street.replace(/[^a-zA-Z ]/g,"");
+        let area=user.Area.replace(/[^a-zA-Z ]/g,"");
+
+        str=str.replace(/ /g,"");
+        area=area.replace(/ /g,"");
+
+        if(str===area){
+            if(str===""){
+                user.Area=null;
+                user.Street=null;                  
+            }
+            else{
+                user.Area=null;
+            }
+        }
 
         
         console.log(user)
@@ -78,10 +102,7 @@ userApi.get("/getaadhaar/:obj",expressErrorHandler(async (req,res,next)=>{
 
 
 
-//dummy route to create protected resourse
-userApi.get("/testing",(req,res)=>{
-    res.send({message:"This is protected data"})
-})
+
 
 
 //export userApi
